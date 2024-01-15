a163c620cb0c8235afcfc7b082d1cc6fc424885e
Nikita <barskii.no@outlook.com>
2024-01-15 16:36:19 +0300

    add checking url, test BeautifulSoup, add requirements

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import requests
 from pathlib import Path
 
 
+def check_for_redirect(response):
+    if response.history and response.url == "https://tululu.org/":
+        raise requests.HTTPError("Book isn't exist")
+
+
 def main():
     book_dir = Path(__file__).parent / "books"
     book_dir.mkdir(parents=True, exist_ok=True)
@@ -9,9 +14,13 @@ def main():
         url = f"https://tululu.org/txt.php?id={book_id}"
         response = requests.get(url)
         response.raise_for_status()
-        filename = book_dir / 'book_{0}.txt'.format(book_id)
-        with open(filename, 'w', encoding='utf-8') as file:
-            file.write(response.text)
+        try:
+            check_for_redirect(response)
+            filename = book_dir / 'book_{0}.txt'.format(book_id)
+            with open(filename, 'w', encoding='utf-8') as file:
+                file.write(response.text)
+        except requests.HTTPError as e:
+            print(f"Error downloading book {book_id}: {e}")  
 
 
 if __name__ == '__main__':
