e88964a3c783c9ea1fb91060845b17baac8ad127
Nikita <barskii.no@outlook.com>
2024-01-16 14:24:43 +0300

    parse books genres

--- main.py
+++ main.py
@@ -53,16 +53,26 @@ def download_image(url, folder='images/'):
         file.write(response.content)
     return filepath
 
+
 def parse_books(url):
+    book_info = {}
     response = requests.get(url)
     response.raise_for_status()
     check_for_redirect(response)
     soup = BeautifulSoup(response.text, 'lxml')
     title_tag_text = soup.find('div', id='content').find('h1').text
-    img_src = soup.find('div', class_='bookimage').find('img')['src']
     title, author = title_tag_text.split("::")
-    title, author = title.strip(), author.strip()
-    return title, author, img_src
+    book_info['title'], book_info['author'] = title.strip(), author.strip()
+    book_info['img_src'] = soup.find('div', class_='bookimage').find('img')['src']
+    comments = []
+    for comment in soup.find_all('div', class_='texts'):
+        comments.append(comment.find('span', class_='black').text)
+    book_info['comments'] = comments
+    genres = []
+    for genre in soup.find('span', class_='d_book').find_all('a'):
+        genres.append(genre.text)
+    book_info['genres'] = genres
+    return book_info
 
 
 def main():
@@ -70,10 +80,10 @@ def main():
         download_url = f'http://tululu.org/txt.php?id={book_id}'
         parse_url = f'https://tululu.org/b{book_id}/'
         try:
-            title, author, img_src = parse_books(parse_url)
-            img_url = urljoin('https://tululu.org', img_src)
-            download_image(img_url)
-            print(title, img_url, sep='\n')
+            book_info = parse_books(parse_url)
+            img_url = urljoin('https://tululu.org', book_info["img_src"])
+            #download_image(img_url)
+            print(book_info["title"], img_url, book_info["genres"], sep='\n')
             #filepath = download_txt(download_url, '{0}. {1}'.format(book_id, parse_books(parse_url)))
         except requests.HTTPError as e:
             print(f"Error with book_id {book_id}: {e}")
