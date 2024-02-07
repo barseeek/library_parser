5e109f859f05ec629a1172e8edf452748f0e4352
Nikita <barskii.no@outlook.com>
2024-02-07 21:14:27 +0300

    Add arguments start_page and end_page, fix error with book_id

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -3,32 +3,45 @@ import requests
 from bs4 import BeautifulSoup
 from main import download_txt, download_image, parse_book_page
 import json
+import argparse
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Download books from tululu.org.")
+    parser.add_argument("--start_page", type=int, default=1)
+    parser.add_argument("--end_page", type=int, default=701)
+    return parser.parse_args()
 
 
-count = 0
-write_to_json=[]
-for page in range(1,3):
-    download_url = 'http://tululu.org/txt.php'
-    url = f'https://tululu.org/l55/{page}'
-    response = requests.get(url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
-    book_tags = soup.select('table.d_book')
-    for book_tag in book_tags:
-        link_tag = book_tag.select_one('a')['href']
-        url_for_parse = urljoin(url, link_tag)
-        try:
-            book_response = requests.get(url_for_parse)
-            book_response.raise_for_status()
-            book_soup = BeautifulSoup(book_response.text, 'lxml')
-            parsed_page = parse_book_page(book_soup)
-            img_url = urljoin(url_for_parse, parsed_page["img_src"])
-            count += 1
-            download_image(img_url)
-            download_txt(download_url, '{0}-я книга. {1}'.format(count, parsed_page['title']))
-            write_to_json.append(parsed_page)
-            print(url_for_parse)
-        except requests.HTTPError as e:
-            print(f"HTTPError with book: {e}")
-with open("books.json", "w", encoding='utf-8') as file:
-    json.dump(write_to_json, file, ensure_ascii=False)
+def main():
+    args = parse_arguments()
+    count = 0
+    write_to_json=[]
+    for page in range(args.start_page, args.end_page + 1):
+        download_url = 'http://tululu.org/txt.php'
+        url = f'https://tululu.org/l55/{page}'
+        response = requests.get(url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, 'lxml')
+        book_tags = soup.select('table.d_book')
+        for book_tag in book_tags:
+            link_tag = book_tag.select_one('a')['href']
+            url_for_parse = urljoin(url, link_tag)
+            book_id = link_tag.strip('b/')
+            try:
+                book_response = requests.get(url_for_parse)
+                book_response.raise_for_status()
+                book_soup = BeautifulSoup(book_response.text, 'lxml')
+                parsed_page = parse_book_page(book_soup)
+                img_url = urljoin(url_for_parse, parsed_page["img_src"])
+                download_image(img_url)
+                download_txt(download_url, '{0}. {1}'.format(book_id, parsed_page['title']))
+                write_to_json.append(parsed_page)
+                print(url_for_parse)
+            except requests.HTTPError as e:
+                print(f"HTTPError with book {book_id}: {e}")
+    with open("books.json", "w", encoding='utf-8') as file:
+        json.dump(write_to_json, file, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    main()
